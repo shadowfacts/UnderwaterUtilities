90d2ac87c06031bdcee0554426c088b194902f71
Shadowfacts <me@shadowfacts.net>
2017-02-05 10:06:08 -0500

    Port to 1.11.2

--- src/main/kotlin/net/shadowfacts/underwaterutilities/UUMaterials.kt
+++ src/main/kotlin/net/shadowfacts/underwaterutilities/UUMaterials.kt
@@ -9,7 +9,7 @@ import net.minecraftforge.common.util.EnumHelper
  */
 object UUMaterials {
 
-	val GOGGLES: ItemArmor.ArmorMaterial = EnumHelper.addArmorMaterial("GOGGLES", "$MOD_ID:goggles", 2, intArrayOf(0, 0, 0, 0), 0, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0f)
-	val SCUBA: ItemArmor.ArmorMaterial = EnumHelper.addArmorMaterial("SCUBA", "$MOD_ID:scuba", 2, intArrayOf(0, 0, 0, 0), 0, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0f)
+	val GOGGLES: ItemArmor.ArmorMaterial = EnumHelper.addArmorMaterial("GOGGLES", "$MOD_ID:goggles", 2, intArrayOf(0, 0, 0, 0), 0, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0f)!!
+	val SCUBA: ItemArmor.ArmorMaterial = EnumHelper.addArmorMaterial("SCUBA", "$MOD_ID:scuba", 2, intArrayOf(0, 0, 0, 0), 0, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0f)!!
 
 }
